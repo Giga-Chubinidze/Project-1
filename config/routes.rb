a6776212ad2ece2828b254808b2a6aed8ed0889d
Giga Chubinidze <gchubinidze@unisens.ge>
2022-03-17 12:25:58 +0400

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  devise_for :users
+  resources :friends
+  #get 'home/index'
+  get 'home/about'
+  #root 'home#index'
+  root 'friends#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
